b4d6284f1778d98e274078cf98f31edf7aae464a
Victor Tran <victortran1449@gmail.com>
2022-11-09 13:30:45 -0800

    do while loops practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,18 @@
-for (i=1; i<11; i++) {
-    console.log(Math.pow(2, i)) //2**i
-}
+// for (i=1; i<11; i++) {
+//     console.log(Math.pow(2, i)) //2**i
+// }
 
-i = 1
-while (i<11) {
-    console.log(Math.pow(2, i)) //2**i
-    i++
-}
+// i = 1
+// while (i<11) {
+//     console.log(Math.pow(2, i)) //2**i
+//     i++
+// }
+
+let input;
+let numberedInput;
+do {
+  input = prompt("What is your name?"); //always return a string
+} while (!isNaN(Number(input))); //users enters a number the result of Number() is a number, else it is Nan
+
+// alert("Hello " + input);
+alert(`Hello ${input}`);
